bc24850ac0eb0c076185831f2079492e10226917
kosyyy <amicarey123@gmail.com>
2022-09-09 13:42:14 +0000

    Task 2 and 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase a-z using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
