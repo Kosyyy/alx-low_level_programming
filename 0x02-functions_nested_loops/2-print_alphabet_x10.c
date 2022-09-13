ac61fe7eb68657309ebd6631d0c42a51b0d7574f
kosyyy <amicarey123@gmail.com>
2022-09-13 16:34:44 +0000

    alpabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,14 @@
 void print_alphabet_x10(void);
 {
 	int j;
+	int x;
 
-	for (j = 'a'; j <= 'z' ; j++)
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
 	{
 		_putchar(j);
 	}
-	_putchar('\n')
+	_putchar('\n');
+	}
 }
