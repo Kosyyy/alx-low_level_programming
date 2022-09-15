5f113c58d533accdce18b7705a410ac2dcdbbf3e
kosyyy <amicarey123@gmail.com>
2022-09-15 12:11:58 +0000

    Task 1 -9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - to print numbers 0 to 9
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
+	return (0);
+}
